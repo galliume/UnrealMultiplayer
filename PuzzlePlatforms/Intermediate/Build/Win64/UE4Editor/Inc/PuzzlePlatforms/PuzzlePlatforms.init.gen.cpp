c17ecdcc9ee1348f9d9106fec67398c693b6a7d1
Guillaume P <gcconantc@gmail.com>
2021-05-13 11:14:33 +0200

    Check if code is running on server or client (hasAuthority)

--- PuzzlePlatforms/Intermediate/Build/Win64/UE4Editor/Inc/PuzzlePlatforms/PuzzlePlatforms.init.gen.cpp
+++ PuzzlePlatforms/Intermediate/Build/Win64/UE4Editor/Inc/PuzzlePlatforms/PuzzlePlatforms.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodePuzzlePlatforms_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x47CA7F78,
-				0x20DADCC1,
+				0x4ACA084F,
+				0x402398D2,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority()) { //if code is running on the server or the client
+		FVector Location = GetActorLocation();
+		Location += FVector(m_Speed * DeltaTime, 0, 0);
+
+		SetActorLocation(Location);
+	}	
+}
